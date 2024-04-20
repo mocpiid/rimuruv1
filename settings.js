6a042026be84606c8ae1b687ec892d5e20a6e877
bratan18137 <167594838+bratan18137@users.noreply.github.com>
2024-04-21 01:23:53 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,16 +21,16 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['4915510605736'] //ur owner number
+global.ownernomer = "4915510605736" //ur owner number2
+global.ownername = "bratan" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
+global.ownernumber = '4915510605736'
 global.ownername = '🦄드림 가이 Xeon'
 global.ownerNumber = ["916909137213@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
